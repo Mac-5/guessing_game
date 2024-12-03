f30cb5c9577b7ac0a9e911402fc804e1082070cf
Mac-5 <emmamarkpius@gmail.com>
2024-12-03 17:24:37 +0100

    Fix tries logic in guessing game: Ensure persistent decrement and game-over check

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,46 @@
 use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
+
 fn main() {
     println!("Guess The Number");
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("Please Input Your Guess");
+    let mut tries = 5; // Define `tries` outside the loop
 
-    let mut guess = String::new();
+    loop {
+        if tries == 0 {
+            println!("You've run out of tries! The secret number was {secret_number}.");
+            break;
+        }
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    let guess: u32 = guess.trim().parse().expect("Please type a number");
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("You win!"),
-    };
+        println!("Please Input Your Guess");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You Guess {guess}");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number.");
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => {
+                tries -= 1;
+                println!("Too small! You have {tries} tries left.");
+            }
+            Ordering::Greater => {
+                tries -= 1;
+                println!("Too big! You have {tries} tries left.");
+            }
+            Ordering::Equal => {
+                println!("You win! The secret number was {secret_number}.");
+                break;
+            }
+        };
+    }
 }
