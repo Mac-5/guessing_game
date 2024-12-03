5a92c02e7b576a22433cc659a7a4f1ab08445347
Mac-5 <emmamarkpius@gmail.com>
2024-12-03 16:57:56 +0100

    Add Random Functionality

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 fn main() {
     println!("Guess The Number");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("Please Input Your Guess");
 
@@ -9,6 +12,12 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+    let guess: u32 = guess.trim().parse().expect("Please type a number");
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("You win!"),
+    };
 
     println!("You Guess {guess}");
 }
